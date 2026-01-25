22dcd85fea253bbbb78953757e424b810088717b
Arpith Paliwal <arpithpaliwal@gmail.com>
2026-01-25 23:11:59 +0530

    fix(mail): refactor dotenv import and enhance environment variable logging

--- Mail/src/index.ts
+++ Mail/src/index.ts
@@ -1,4 +1,14 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+  path: path.resolve(process.cwd(), ".env"),
+});
+console.log("MAIL ENV CHECK:", {
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASS_LENGTH: process.env.EMAIL_PASS?.length,
+});
+
 import { connectRabbitMQ } from "./rabbitmq/connection.rabbitmq.js";
 import { startMailConsumer } from "./rabbitmq/consumer.rabbitmq.js";
 
